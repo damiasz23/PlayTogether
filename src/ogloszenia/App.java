4b43ab576fba485ce802446f2a725882dfa61b4f
RENT <damiasz@op.pl>
2017-10-23 20:05:07 +0200

    Login

--- src/ogloszenia/App.java
+++ src/ogloszenia/App.java
@@ -1,6 +1,11 @@
 package ogloszenia;
 
 import ogloszeniar.hibernate.util.HibernateUtil;
+import playTogether.Player;
+import playTogether.User;
+import playTogether.repositorys.PlayerRepository;
+
+import java.time.LocalDateTime;
 
 /**
  * Created by Lukasz on 27.09.2017.
@@ -9,6 +14,9 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         HibernateUtil.openSession();
+
+        User user = new User("dam@op.pl", "1234");
+
 
     }
 }
